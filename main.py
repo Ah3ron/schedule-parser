5c515f125d0305afb008cd0dc3b7067ed71b4321
Ah3ron <ah3ron@protonmail.com>
2024-02-18 14:54:13 +0300

    Added extract week start dates from HTML

--- main.py
+++ main.py
@@ -34,11 +34,27 @@ def extract_group_array(content):
     return result
 
 
+def extract_week_start_dates(content):
+    soup = BeautifulSoup(content, "lxml")
+    week_dates = {}
+    for li in soup.find("ul", id="weeks-menu").find_all("li"):
+        if not li.a:
+            continue
+
+        if li.a["href"] == "#":
+            continue
+
+        week_num = li.a["href"][2:]
+        week_dates[week_num] = li.a.text.split("(")[1][:5]
+    return week_dates
+
+
 async def main():
-    url = "https://www.polessu.by/ruz/"
+    url = "https://www.polessu.by/ruz/term2/?q=22%D0%98%D0%A2-1"
     content = await get_page_content(url)
-    groups = extract_group_array(content)
-    print(groups)
+    # groups = extract_group_array(content)
+    week_start_dates = extract_week_start_dates(content)
+    print(week_start_dates)
 
 
 if __name__ == "__main__":
