d6db786d7c92d9679bfebe975ef43607667e09e1
Ah3ron <ah3ron@protonmail.com>
2024-06-05 17:34:30 +0300

    Code refactoring

--- main.py
+++ main.py
@@ -10,130 +10,69 @@ import asyncpg
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
+# Logging configuration
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 logger = logging.getLogger(__name__)
 
+# Constants
+WEEKDAY_TO_NUMBER = {
+    "Понедельник": 0,
+    "Вторник": 1,
+    "Среда": 2,
+    "Четверг": 3,
+    "Пятница": 4,
+    "Суббота": 5,
+    "Воскресенье": 6,
+}
+BASE_URL = "https://www.polessu.by/ruz/"
 
+
+# Utility functions
 def calculate_date(week_start_date, day_of_week):
     date = datetime.strptime(week_start_date, "%d.%m").replace(year=datetime.now().year)
-    date += timedelta(days=convert_day_to_number(day_of_week))
+    date += timedelta(days=WEEKDAY_TO_NUMBER[day_of_week])
     return date.strftime("%d.%m")
 
 
-def convert_day_to_number(day_of_week):
-    day_numbers = {
-        "Понедельник": 0,
-        "Вторник": 1,
-        "Среда": 2,
-        "Четверг": 3,
-        "Пятница": 4,
-        "Суббота": 5,
-        "Воскресенье": 6,
-    }
-
-    return day_numbers.get(day_of_week, -1)
-
-
-async def get_or_create_table(conn):
-    exists_lessons = await conn.fetchval(
-        """
-        SELECT EXISTS (
-            SELECT 1
-            FROM information_schema.tables
-            WHERE table_name = 'lessons'
-        )
-        """
-    )
-    if not exists_lessons:
-        await conn.execute(
-            """
-            CREATE TABLE lessons (
-                id SERIAL PRIMARY KEY,
-                group_name VARCHAR(16) NOT NULL,
-                lesson_date VARCHAR(8) NOT NULL,
-                day_of_week VARCHAR(12) NOT NULL,
-                lesson_time VARCHAR(16) NOT NULL,
-                lesson_name VARCHAR(192) NOT NULL,
-                location VARCHAR(32),
-                teacher VARCHAR(384),
-                subgroup VARCHAR(16)
-            );
-            """
-        )
-        await conn.execute(
-            """
-            CREATE INDEX idx_lesson_date_and_group_name ON lessons (lesson_date, group_name);
-            """
-        )
-
-    exists_schedule_updates = await conn.fetchval(
-        """
-        SELECT EXISTS (
-            SELECT 1
-            FROM information_schema.tables
-            WHERE table_name = 'schedule_updates'
-        )
-        """
-    )
-    if not exists_schedule_updates:
-        await conn.execute(
-            """
-            CREATE TABLE schedule_updates (
-                id SERIAL PRIMARY KEY,
-                last_update_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-            );
-            """
-        )
-
-
-async def fetch_page_content(session, url):
-    for _ in range(3):
+async def fetch_content_with_retries(session, url, retries=3, delay=1):
+    for _ in range(retries):
         try:
             async with session.get(url) as response:
-                content = await response.text()
-                return content
+                return await response.text()
         except aiohttp.ClientError as e:
             logger.error(f"Error fetching page content: {e}")
-            await asyncio.sleep(1)
+            await asyncio.sleep(delay)
     return None
 
 
-def extract_group_array(soup):
-    result = []
+def extract_group_list(soup):
     script_tag = soup.find("script", src=False)
     if not script_tag:
-        return result
-
+        return []
     script_content = script_tag.string
-    start_index = script_content.find("var query = [")
-    end_index = script_content.find("];", start_index)
-
-    if start_index == -1 or end_index == -1:
-        return result
-
-    query_string = script_content[start_index + len("var query = ") : end_index + 1]
-    query_array = eval(query_string)
-    result = [
+    query_string = re.search(r"var query = (\[.*?\]);", script_content)
+    if not query_string:
+        return []
+    query_array = eval(query_string.group(1))
+    return [
         item.strip()
         for item in query_array
         if item and item[0].isdigit() and "(" not in item and ")" not in item
     ]
-    return result
 
 
-def get_week_start_dates(soup):
+def extract_week_start_dates(soup):
     week_start_dates = {}
     for li in soup.find("ul", id="weeks-menu").find_all("li"):
-        if not li.a or li.a["href"] == "#":
-            continue
-        week_num = li.a["href"][2:]
-        week_start_dates[week_num] = li.a.text.split("(")[1][:5]
+        if li.a and li.a["href"] != "#":
+            week_num = li.a["href"][2:]
+            week_start_dates[week_num] = li.a.text.split("(")[1][:5]
     return week_start_dates
 
 
-def extract_lessons(table, week_start_dates):
+def extract_lessons_from_table(table, week_start_dates):
     schedule = []
     current_day_of_week = None
 
@@ -147,7 +86,6 @@ def extract_lessons(table, week_start_dates):
                 continue
 
             lesson_info = row.find_all("td")
-
             lesson = {
                 "date": calculate_date(
                     week_start_dates[class_name[1:]], current_day_of_week
@@ -159,7 +97,6 @@ def extract_lessons(table, week_start_dates):
                 "teacher": lesson_info[3].text or None,
                 "subgroup": lesson_info[4].text or None,
             }
-
             schedule.append(lesson)
 
     return schedule
@@ -171,72 +108,88 @@ async def parse_schedule(content):
     if not table:
         logger.warning("Failed to find schedule on the page")
         return None
-
-    week_start_dates = get_week_start_dates(soup)
-    schedule = extract_lessons(table, week_start_dates)
-    return schedule
+    week_start_dates = extract_week_start_dates(soup)
+    return extract_lessons_from_table(table, week_start_dates)
 
 
-async def extract_schedule(session, group):
+async def fetch_group_schedule(session, group):
     urls = [
-        f"https://www.polessu.by/ruz/term2/?q={group}&f=1",
-        f"https://www.polessu.by/ruz/term2/?q={group}&f=2",
-        f"https://www.polessu.by/ruz/?q={group}&f=1",
-        f"https://www.polessu.by/ruz/?q={group}&f=2",
+        f"{BASE_URL}term2/?q={group}&f=1",
+        f"{BASE_URL}term2/?q={group}&f=2",
+        f"{BASE_URL}?q={group}&f=1",
+        f"{BASE_URL}?q={group}&f=2",
     ]
-    all_schedules = {}
-
     for url in urls:
-        content = await fetch_page_content(session, url)
+        content = await fetch_content_with_retries(session, url)
         if content:
             schedule = await parse_schedule(content)
             if schedule:
-                all_schedules[group] = schedule
                 logger.info(f"Schedule for group {group} successfully fetched")
-                break
+                return group, schedule
         else:
             logger.error(f"Failed to fetch page content for group {group}")
-
-    return group, all_schedules.get(group)
+    return group, None
 
 
 async def fetch_last_update_date(session, url):
-    async def get_last_update_date(url):
-        content = await fetch_page_content(session, url)
+    async def get_update_date(url):
+        content = await fetch_content_with_retries(session, url)
         if not content:
-            logger.error(f"Failed to fetch content from the page: {url}")
             return None
-
         soup = BeautifulSoup(content, "lxml")
-        last_update_tag = soup.find("div", class_="container").find("p", class_="small")
-
-        if last_update_tag:
+        update_tag = soup.find("div", class_="container").find("p", class_="small")
+        if update_tag:
             date_match = re.search(
-                r"\d{2}\.\d{2}\.\d{4} \d{2}:\d{2}", last_update_tag.text.strip()
+                r"\d{2}\.\d{2}\.\d{4} \d{2}:\d{2}", update_tag.text.strip()
             )
             if date_match:
                 return datetime.strptime(date_match.group(), "%d.%m.%Y %H:%M")
-
-        logger.error(f"Failed to find last update information on the page: {url}")
         return None
 
-    main_date = await get_last_update_date(url)
-    term2_date = await get_last_update_date(url + "term2")
+    main_date = await get_update_date(url)
+    term2_date = await get_update_date(url + "term2")
+    return (
+        max(main_date, term2_date)
+        if main_date and term2_date
+        else main_date or term2_date
+    )
 
-    if main_date and term2_date:
-        return max(main_date, term2_date)
-    return main_date or term2_date
+
+async def create_tables_if_not_exists(conn):
+    await conn.execute(
+        """
+        CREATE TABLE IF NOT EXISTS lessons (
+            id SERIAL PRIMARY KEY,
+            group_name VARCHAR(16) NOT NULL,
+            lesson_date VARCHAR(8) NOT NULL,
+            day_of_week VARCHAR(12) NOT NULL,
+            lesson_time VARCHAR(16) NOT NULL,
+            lesson_name VARCHAR(192) NOT NULL,
+            location VARCHAR(32),
+            teacher VARCHAR(384),
+            subgroup VARCHAR(16)
+        );
+        CREATE INDEX IF NOT EXISTS idx_lesson_date_and_group_name ON lessons (lesson_date, group_name);
+        """
+    )
+    await conn.execute(
+        """
+        CREATE TABLE IF NOT EXISTS schedule_updates (
+            id SERIAL PRIMARY KEY,
+            last_update_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+        );
+        """
+    )
 
 
 async def main():
     load_dotenv()
     conn = await asyncpg.connect(os.getenv("DATABASE_URL"))
 
-    await get_or_create_table(conn)
+    await create_tables_if_not_exists(conn)
 
-    url = "https://www.polessu.by/ruz/"
     async with aiohttp.ClientSession() as session:
-        last_update_date = await fetch_last_update_date(session, url)
+        last_update_date = await fetch_last_update_date(session, BASE_URL)
         if not last_update_date:
             logger.error("Failed to fetch last update date")
             await conn.close()
@@ -246,68 +199,67 @@ async def main():
             "SELECT MAX(last_update_date) FROM schedule_updates"
         )
 
-        if stored_last_update_date and (last_update_date == stored_last_update_date):
+        if stored_last_update_date and last_update_date <= stored_last_update_date:
             logger.info("No new schedule update available")
-        else:
-            content = await fetch_page_content(session, url)
-            if not content:
-                logger.error("Failed to fetch content from the main page")
-                await conn.close()
-                return
+            await conn.close()
+            return
 
-            soup = BeautifulSoup(content, "lxml")
-            group_array = extract_group_array(soup)
+        content = await fetch_content_with_retries(session, BASE_URL)
+        if not content:
+            logger.error("Failed to fetch content from the main page")
+            await conn.close()
+            return
 
-            tasks = [extract_schedule(session, group) for group in group_array]
+        soup = BeautifulSoup(content, "lxml")
+        group_list = extract_group_list(soup)
 
-            results = await asyncio.gather(*tasks)
+        tasks = [fetch_group_schedule(session, group) for group in group_list]
+        results = await asyncio.gather(*tasks)
 
-            batch_data = []
-
-            for group, schedule in results:
-                if schedule:
-                    for lesson in schedule:
-                        batch_data.append(
-                            (
-                                group,
-                                lesson["date"],
-                                lesson["day_of_week"],
-                                lesson["time"],
-                                lesson["name"],
-                                lesson["location"],
-                                lesson["teacher"],
-                                lesson["subgroup"],
-                            )
-                        )
-
-            if batch_data:
-                await conn.execute("DELETE FROM lessons")
-                start = time.time()
-                await conn.executemany(
-                    """
-                        INSERT INTO lessons (
-                            group_name, 
-                            lesson_date, 
-                            day_of_week, 
-                            lesson_time, 
-                            lesson_name, 
-                            location, 
-                            teacher, 
-                            subgroup
-                        ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-                        """,
-                    batch_data,
-                )
-                logger.info(
-                    f"The new schedule was saved in {time.time() - start:.2f} seconds\n"
-                )
-
-            await conn.execute(
-                """
-                    INSERT INTO schedule_updates (last_update_date) VALUES ($1)
-                    """,
-                last_update_date,
+        batch_data = [
+            (
+                group,
+                lesson["date"],
+                lesson["day_of_week"],
+                lesson["time"],
+                lesson["name"],
+                lesson["location"],
+                lesson["teacher"],
+                lesson["subgroup"],
             )
+            for group, schedule in results
+            if schedule
+            for lesson in schedule
+        ]
+
+        if batch_data:
+            await conn.execute("DELETE FROM lessons")
+            start = time.time()
+            await conn.executemany(
+                """
+                INSERT INTO lessons (
+                    group_name, 
+                    lesson_date, 
+                    day_of_week, 
+                    lesson_time, 
+                    lesson_name, 
+                    location, 
+                    teacher, 
+                    subgroup
+                ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+                """,
+                batch_data,
+            )
+            logger.info(
+                f"The new schedule was saved in {time.time() - start:.2f} seconds"
+            )
+
+        await conn.execute(
+            """
+            INSERT INTO schedule_updates (last_update_date) VALUES ($1)
+            """,
+            last_update_date,
+        )
 
     await conn.close()
 
